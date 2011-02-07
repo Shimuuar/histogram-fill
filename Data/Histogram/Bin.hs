ac5757e3596ae3f515f095d13dfba4d47237333f
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-02-07 16:13:15 +0300

    Refactor LogBinD

--- Data/Histogram/Bin.hs
+++ Data/Histogram/Bin.hs
@@ -510,48 +510,44 @@ instance Read BinD where
 --
 -- 1. Lower bound
 --
--- 2. Upper bound
---
 -- 2. Increment ratio
 --
 -- 3. Number of bins
 data LogBinD = LogBinD
                Double -- Low border
-               Double -- Hi border
                Double -- Increment ratio
                Int    -- Number of bins
                deriving (Eq,Typeable)
 
 -- | Create log-scale bins.
 logBinD :: Double -> Int -> Double -> LogBinD
-logBinD lo n hi = LogBinD lo hi ((hi/lo) ** (1 / fromIntegral n)) n
+logBinD lo n hi = LogBinD lo ((hi/lo) ** (1 / fromIntegral n)) n
 
 instance Bin LogBinD where
   type BinValue LogBinD = Double
-  toIndex   !(LogBinD base _ step _) !x = floorD $ logBase step (x / base)
-  fromIndex !(LogBinD base _ step _) !i | i >= 0    = base * step ** (fromIntegral i + 0.5)
+  toIndex   !(LogBinD base step _) !x = floorD $ logBase step (x / base)
+  fromIndex !(LogBinD base step _) !i | i >= 0    = base * step ** (fromIntegral i + 0.5)
                                         | otherwise = -1 / 0
-  inRange   !(LogBinD lo hi _ _) x  = x >= lo && x < hi
-  nBins     !(LogBinD _ _ _ n) = n
+  nBins     !(LogBinD _ _ n) = n
   {-# INLINE toIndex #-}
 
 instance IntervalBin LogBinD where
-  binInterval (LogBinD base _ step _) i = (x, x*step) where x = base * step ** (fromIntegral i)
+  binInterval (LogBinD base step _) i = (x, x*step) where x = base * step ** (fromIntegral i)
 
 instance Bin1D LogBinD where
-  lowerLimit (LogBinD lo _  _ _) = lo
-  upperLimit (LogBinD _  hi _ _) = hi
-  unsafeSliceBin i j (LogBinD from q step _) = LogBinD (from * step ^ i) q step (j-i+1)
+  lowerLimit (LogBinD lo  _ _) = lo
+  upperLimit (LogBinD lo  r n) = lo * r ^ n
+  unsafeSliceBin i j (LogBinD from step _) = LogBinD (from * step ^ i) step (j-i+1)
 
 instance VariableBin LogBinD where
-  binSizeN (LogBinD base _ step _) n = let x = base * step ^ n in x*step - x
+  binSizeN (LogBinD base step _) n = let x = base * step ^ n in x*step - x
 
 instance Show LogBinD where
-  show (LogBinD lo hi _ n) =
+  show b =
     unlines [ "# LogBinD"
-            , "# Lo   = " ++ show lo
-            , "# N    = " ++ show n
-            , "# Hi   = " ++ show hi
+            , "# Lo   = " ++ show (lowerLimit b)
+            , "# N    = " ++ show (nBins b)
+            , "# Hi   = " ++ show (upperLimit b)
             ]
 instance Read LogBinD where
   readPrec = do
