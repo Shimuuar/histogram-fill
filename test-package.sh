ebfce7e244877b040ac0990a102a0c3f3011d1e8
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2014-02-22 16:10:31 +0400

    Treat tests specially during CI

--- test-package.sh
+++ test-package.sh
@@ -2,21 +2,25 @@
 set -e # Bail on first error
 set -v # Be verbose
 
-cd "$1"
-
-# Install dependencies, build and run test suite
-cabal-1.18 install   --enable-tests --only-dependencies
-cabal-1.18 configure --enable-tests -v2  # -v2 provides useful information for debugging
-cabal-1.18 build
-cabal-1.18 test
-
-# Check that tarball could be built and installed
-cabal-1.18 sdist
-SRC_TGZ=$(cabal-1.18 info . | awk '{print $2 ".tar.gz";exit}') ;
-cd dist/
-if [ -f "$SRC_TGZ" ]; then
-    cabal-1.18 install "$SRC_TGZ"
+if "x$1" = histogram-fill-tests; then
+    cabal-1.18 configure --enable-tests -v2  # -v2 provides useful information for debugging
+    cabal-1.18 test
 else
-    echo "expected '$SRC_TGZ' not found"
-    exit 1
+    cd "$1"
+
+    # Install dependencies, build and run test suite
+    cabal-1.18 install   --only-dependencies
+    cabal-1.18 configure -v2  # -v2 provides useful information for debugging
+    cabal-1.18 build
+
+    # Check that tarball could be built and installed
+    cabal-1.18 sdist
+    SRC_TGZ=$(cabal-1.18 info . | awk '{print $2 ".tar.gz";exit}') ;
+    cd dist/
+    if [ -f "$SRC_TGZ" ]; then
+	cabal-1.18 install "$SRC_TGZ"
+    else
+	echo "expected '$SRC_TGZ' not found"
+	exit 1
+    fi
 fi
