e1ce17c8111ea55f274de50bfb5505477f4c959b
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2015-01-04 00:25:59 +0300

    Update travis script

--- test-package.sh
+++ test-package.sh
@@ -3,22 +3,22 @@ set -e # Bail on first error
 set -v # Be verbose
 
 if [ "x$1" = histogram-fill-tests ]; then
-    cabal-1.18 configure --enable-tests -v2  # -v2 provides useful information for debugging
-    cabal-1.18 test
+    cabal configure --enable-tests -v2  # -v2 provides useful information for debugging
+    cabal test
 else
     cd "$1"
 
     # Install dependencies, build and run test suite
-    cabal-1.18 install   --only-dependencies
-    cabal-1.18 configure -v2  # -v2 provides useful information for debugging
-    cabal-1.18 build
+    cabal install   --only-dependencies
+    cabal configure -v2  # -v2 provides useful information for debugging
+    cabal build
 
     # Check that tarball could be built and installed
-    cabal-1.18 sdist
-    SRC_TGZ=$(cabal-1.18 info . | awk '{print $2 ".tar.gz";exit}') ;
+    cabal sdist
+    SRC_TGZ=$(cabal info . | awk '{print $2 ".tar.gz";exit}') ;
     cd dist/
     if [ -f "$SRC_TGZ" ]; then
-	cabal-1.18 install "$SRC_TGZ"
+	cabal install "$SRC_TGZ"
     else
 	echo "expected '$SRC_TGZ' not found"
 	exit 1
